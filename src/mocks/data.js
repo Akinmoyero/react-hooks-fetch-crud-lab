be872907fad91fe7bf9842083df9236f8ea3036d
Akinmoyero <akinmoyeroboluwatife3@gmail.com>
2025-02-04 10:15:58 +0300

    lab

--- src/mocks/data.js
+++ src/mocks/data.js
@@ -1,14 +1,16 @@
-export const data = [
-  {
-    id: 1,
-    prompt: "lorem testum 1",
-    answers: ["choice 1", "choice 2", "choice 3", "choice 4"],
-    correctIndex: 0,
-  },
-  {
-    id: 2,
-    prompt: "lorem testum 2",
-    answers: ["choice 1", "choice 2", "choice 3", "choice 4"],
-    correctIndex: 2,
-  },
-];
+{
+  "questions" [
+    {
+      "id": 1,
+      "prompt": "lorem testum 1",
+      "answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+      "correctIndex": 0
+    },
+    {
+      "id": 2,
+      "prompt": "lorem testum 2",
+      "answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+      "correctIndex": 1
+    }
+  ]
+}
